5043bbc0b6277ed11492381d33a0fa09ee1ef1a4
Hamish Coleman <hamish@zot.org>
2016-12-02 20:31:30 +0800

    Add a simple persistance log to the seen list handler

--- lib/SeenList.pm
+++ lib/SeenList.pm
@@ -5,11 +5,56 @@ use strict;
 # Keep track of which files have been watched
 #
 
+use File::Path qw(make_path);
+use FileHandle;
+
 sub new {
     my $class = shift;
     my $self = {};
     bless($self, $class);
+    $self->replay_log();
     return $self;
+}
+
+sub _validate_path {
+    my $self = shift;
+    my $dirname = $ENV{'HOME'}.'/.cache/media-lister';
+    make_path($dirname);
+    my $filename = $dirname.'/seenlog';
+    return $filename;
+}
+
+sub log {
+    my $self = shift;
+    my $name = shift;
+    my $value = shift;
+
+    # open the file
+    if (!defined($self->{fh})) {
+        my $filename = $self->_validate_path();
+        my $fh = FileHandle->new($filename,"a");
+        return undef if (!defined($fh));
+        $self->{fh} = $fh;
+    }
+
+    if (!$value) {
+        $value = 0;
+    }
+    $self->{fh}->write($value."\t".$name."\n");
+    $self->{fh}->flush();
+}
+
+sub replay_log {
+    my $self = shift;
+    my $filename = $self->_validate_path();
+    my $fh = FileHandle->new($filename,"r");
+    return undef if (!defined($fh));
+    while (<$fh>) {
+        chomp;
+        if ($_ =~ /^([^\t]+)\t(.*)$/) {
+            $self->seen($2,$1,'nolog');
+        }
+    }
 }
 
 # TODO
@@ -20,9 +65,13 @@ sub seen {
     my $self = shift;
     my $name = shift;
     my $value = shift;
+    my $nolog = shift;
 
     if (defined($value)) {
         $self->{seen}{$name} = $value;
+        if (!defined($nolog)) {
+            $self->log($name,$value);
+        }
     }
 
     return $self->{seen}{$name};
